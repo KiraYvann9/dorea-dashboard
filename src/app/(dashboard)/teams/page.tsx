99fec2ecabcae7ea28bfa9d99105d7e4459cfa60
KiraYvann9 <kludovic09@gmail.com>
2024-12-01 00:03:05 +0000

    Feat: init du formulaire des rôles

--- src/app/(dashboard)/teams/page.tsx
+++ src/app/(dashboard)/teams/page.tsx
@@ -1,7 +1,4 @@
-import { Button } from "@/components/ui/button"
 
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
     Tabs,
     TabsContent,
@@ -11,6 +8,7 @@ import {
 
 import {FormComponents} from "@/app/(dashboard)/teams/_components/FormComponent";
 import {DepartementForm} from "@/app/(dashboard)/teams/_components/DepartementForm";
+import {RoleForm} from "@/app/(dashboard)/teams/_components/RoleForm";
 
 
 export default function Dashboard() {
@@ -19,9 +17,10 @@ export default function Dashboard() {
             <h1 className={'text-3xl font-semibold text-gray-700'}>Equipe & Départements</h1>
 
             <Tabs defaultValue={'equipes'} className={'w-full '}>
-                <TabsList className={'w-full flex justify-start bg-gray-200 border'}>
-                    <TabsTrigger value={'equipes'} className={'text-xl px-6'}>Equipes</TabsTrigger>
-                    <TabsTrigger value={'departements'} className={'text-xl px-6'}>Départements</TabsTrigger>
+                <TabsList className={'w-full flex justify-start rounded-none bg-gray-200 border-b-2 border-custom_dark'}>
+                    <TabsTrigger value={'equipes'} className={'text-xl sm:px-6'}>Equipes</TabsTrigger>
+                    <TabsTrigger value={'departements'} className={'text-xl sm:px-6'}>Départements</TabsTrigger>
+                    <TabsTrigger value={'roles'} className={'text-xl sm:px-6'}>Roles</TabsTrigger>
                 </TabsList>
 
                 <TabsContent value={'equipes'} className={'px-6 space-y-8'}>
@@ -34,6 +33,12 @@ export default function Dashboard() {
                     <DepartementForm/>
                     <div className={'flex-1 w-full rounded-md bg-white space-y-16'}>
                         <h3 className={'text-2xl font-semibold text-muted-foreground'}>Liste des départements</h3>
+                    </div>
+                </TabsContent>
+                <TabsContent value={'roles'} className={'px-6 space-y-8'}>
+                    <RoleForm/>
+                    <div className={'flex-1 w-full rounded-md bg-white space-y-16'}>
+                        <h3 className={'text-2xl font-semibold text-muted-foreground'}>Liste des Roles</h3>
                     </div>
                 </TabsContent>
             </Tabs>
